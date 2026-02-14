9dc4af9a499d78de6993b4185fdc08c55cc25fc8
kartik kumar <kartikkumarsharma15@gmail.com>
2026-02-14 15:28:43 +0530

    04-card-project

---
+++ components/card.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+
+const Card = (props) => {
+    console.log(props.company);
+
+  return (
+    <div >
+      <div className="card">
+              <div>
+                <div className="top">
+                <img src={props.brandLogo} alt=""/>
+                <button>save<Bookmark  size={10} /></button>
+              </div>
+      
+              <div className="center">
+                  <h3>{props.company} <span>{props.datePosted}</span></h3>
+                  <h2>{props.post}</h2>
+                  <div className="tag">
+                    <h4>{props.tag1} </h4>
+                    <h4>{props.tag2}</h4>
+                  </div>
+              </div>
+              </div>
+      
+              <div className="bottom">
+                  <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                  </div>
+                    <button>apply now</button>
+              </div>
+      
+              
+     </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ components/user.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const User = (props) => {
+  return (
+    <div style={{color:'white'}}>
+      {/* {props.name} */}
+      <h1>kartik</h1>
+    </div>
+  )
+}
+
+export default User
